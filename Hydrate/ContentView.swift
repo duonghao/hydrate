1071d0300663ea0715e9310803babc370ee8c338
Hao Duong <hao.duong11@gmail.com>
2023-10-24 15:23:27 +0900

    Initial Commit

---
+++ Hydrate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hydrate
+//
+//  Created by Hao Duong on 24/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Hao Duong on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
